3f67738ebe8c47055c0aee49811c380759d2e208
Megan Reardon <meganr@gmail.com>
2016-06-29 11:37:30 -0700

    committing pages for merge

--- code/scripts/controllers/infoController.js
+++ code/scripts/controllers/infoController.js
@@ -2,7 +2,9 @@
   var infoController = {};
 
   infoController.index = function() {
-    
+    // hide the list #
+    // show the single permit #
+    // call initAutocomplete
   };
 
   module.infoController = infoController;
