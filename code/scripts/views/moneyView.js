4b4051841eb24580fc83a5120ea290811fbc78bc
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-01 11:32:25 -0700

    updates to index.html, readded pin drop animation and formatted value into dollars

--- code/scripts/views/moneyView.js
+++ code/scripts/views/moneyView.js
@@ -9,19 +9,21 @@
     var sortedByValue = Permit.all.sort(function(a, b) {
       return b.value - a.value;
     });
+    var temp = sortedByValue.slice(0, 20);
+    temp.forEach(function(a) {
+      a.value = formatDollar(a.value);
+      $('#money-container').append(moneyPermitsCompiler(a));
+    });
     console.log(sortedByValue);
-    for (var i = 0; i < 20; i++) {
-      $('#money-container').append(moneyPermitsCompiler(sortedByValue[i]));
-    }
+  };
+
+  // A function to format the value to $
+  function formatDollar(num) {
+    var p = num.toFixed(2).split('.');
+    return '$' + p[0].split('').reverse().reduce(function(acc, num, i, orig) {
+      return num + (i && !(i % 3) ? ',' : '') + acc;
+    }, '') + '.' + p[1];
   };
 
   module.moneyView = moneyView;
 })(window);
-//
-//  A function to format the value to $
-// function formatDollar(num) {
-//     var p = num.toFixed(2).split(".");
-//     return "$" + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
-//         return  num + (i && !(i % 3) ? "," : "") + acc;
-//     }, "") + "." + p[1];
-// }
